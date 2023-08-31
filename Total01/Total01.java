b98f8daa4a814ee0180e4c98e3c2d7c116caa1bf
Nakamura <kisy23013@kibireha.ac.jp>
2023-08-31 11:18:49 +0900

    Total01.javaを追加

---
+++ Total01/Total01.java
@@ -0,0 +1,7 @@
+public class Total01 {
+    public static void main(String[] args) {
+        int sum;
+        sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+        System.out.println(sum);
+    }
+}
