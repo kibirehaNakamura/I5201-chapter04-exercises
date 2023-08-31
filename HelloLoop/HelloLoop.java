d8057e654061fbb5e3a9caea8fa6f8cc08420a24
Nakamura <kisy23013@kibireha.ac.jp>
2023-08-31 11:37:14 +0900

    HelloLoop.javaを追加

---
+++ HelloLoop/HelloLoop.java
@@ -0,0 +1,11 @@
+public class HelloLoop {
+	public static void main(String[] args) {
+		int i = 0;
+		
+		while(i <= 4) {
+			System.out.println("Hello!");
+			i = i + 1;
+		}
+		//"Hello!"は5回表示される
+	}
+}
