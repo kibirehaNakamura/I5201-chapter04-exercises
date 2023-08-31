c4b8bd2f324424c531623896897fb9e8da7675d3
Nakamura <kisy23013@kibireha.ac.jp>
2023-08-31 11:22:37 +0900

    Loop1.javaを追加

---
+++ Loop1/Loop1.java
@@ -0,0 +1,10 @@
+public class Loop1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i < 3) {
+            System.out.println("iの値は" + i);
+            i = i + 1;
+        }
+        System.out.println("iの最後の値は" + i);
+    }
+}
