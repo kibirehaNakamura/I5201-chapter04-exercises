10e0fcdd418bdc889788397b87c497566f183051
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-12 13:45:59 +0900

    ExamLoop.javaを追加

---
+++ ExamLoop/ExamLoop.java
@@ -0,0 +1,11 @@
+public class ExamLoop {
+	public static void main(String[] args) {
+		int num = 8;
+		
+		// カウンタの宣言をfor内に変更
+		for(int i = 0; i < num; i++) {
+			System.out.print(".");
+		}
+		System.out.println();
+	}
+}
