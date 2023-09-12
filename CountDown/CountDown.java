99edfd55503949abe82640b5998d98c14f6fff4c
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-12 13:25:39 +0900

    CountDown.javaを追加

---
+++ CountDown/CountDown.java
@@ -0,0 +1,9 @@
+public class CountDown {
+	public static void main(String[] args) {
+		// カウンタ宣言部分をfor内に変更
+		for(int i = 5; i >= 0; i--) {
+			System.out.println(i);
+		}
+		System.out.println("Start!!");
+	}
+}
