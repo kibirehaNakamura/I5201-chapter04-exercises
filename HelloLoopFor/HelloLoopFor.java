d1a26fe6df2e6aa51272c99bf404360282b185c0
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-12 13:22:18 +0900

    HelloLoopFor.javaを追加

---
+++ HelloLoopFor/HelloLoopFor.java
@@ -0,0 +1,8 @@
+public class HelloLoopFor {
+	public static void main(String[] args) {
+		//カウンタ宣言部分をfor内に変更
+		for(int i = 5; i > 0; i--) {
+			System.out.println("Hello!");	//Hello!は5回表示される
+		}
+	}
+}
