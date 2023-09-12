41fb0be26e0f648e5904f076400efa14f0883cfa
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-12 13:18:41 +0900

    Total03.javaを追加

---
+++ Total03/Total03.java
@@ -0,0 +1,12 @@
+public class Total03 {
+	public static void main(String[] args) {
+		int i;
+		int sum;
+		
+		sum = 0;
+		for(i = 1; i <= 10; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
